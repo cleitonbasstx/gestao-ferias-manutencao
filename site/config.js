52f0fa1ebc568e951070e1ad0e6ce53df000ce84
cleitonbasstx <cleitonbasstx@gmail.com>
2026-08-31 22:27:48 -0300

    Create config.js

---
+++ site/config.js
@@ -0,0 +1,6 @@
+// Public configuration only. NEVER put a service_role/secret key here.
+window.SUPABASE_CONFIG = {
+  url: 'https://ejakaaehwbzjlclkcrcp.supabase.co',
+  // Legacy anon key is intentionally public and limited by RLS/RPC grants.
+  publishableKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqYWthYWVod2J6amxjbGtjcmNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgyMDQ0NTYsImV4cCI6MjEwMzc4MDQ1Nn0.b4NA7tsmLXTbnnihH-GXZnCmRG5ykO-ixJzCng6mF1M'
+};
